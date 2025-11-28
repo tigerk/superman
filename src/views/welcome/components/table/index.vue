8c519dddd9e5817977a5c91d9bbd8b0495ebd353
tigerk <kimhwawoon@gmail.com>
2025-11-28 11:35:52 +0800

    Add HomeController and update various Vue components
    Add a new `HomeController` for handling home requests. Update several Vue components to improve code formatting, adjust styles, and modify data values.

--- src/views/welcome/components/table/index.vue
+++ src/views/welcome/components/table/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-  import { useColumns } from "./columns";
-  import { useRenderIcon } from "@/components/ReIcon/src/hooks";
+import { useColumns } from "./columns";
+import { useRenderIcon } from "@/components/ReIcon/src/hooks";
 
-  const { loading, columns, dataList, pagination, Empty, onCurrentChange } = useColumns();
+const { loading, columns, dataList, pagination, Empty, onCurrentChange } =
+  useColumns();
 </script>
 
 <template>
@@ -12,7 +13,12 @@
     showOverflowTooltip
     :loading="loading"
     :loading-config="{ background: 'transparent' }"
-    :data="dataList.slice((pagination.currentPage - 1) * pagination.pageSize, pagination.currentPage * pagination.pageSize)"
+    :data="
+      dataList.slice(
+        (pagination.currentPage - 1) * pagination.pageSize,
+        pagination.currentPage * pagination.pageSize
+      )
+    "
     :columns="columns"
     :pagination="pagination"
     @page-current-change="onCurrentChange"
@@ -25,35 +31,41 @@
       </el-empty>
     </template>
     <template #operation="{ row }">
-      <el-button plain circle size="small" :title="`查看序号为${row.id}的详情`" :icon="useRenderIcon('ri:search-line')" />
+      <el-button
+        plain
+        circle
+        size="small"
+        :title="`查看序号为${row.id}的详情`"
+        :icon="useRenderIcon('ri:search-line')"
+      />
     </template>
   </pure-table>
 </template>
 
 <style lang="scss">
-  .pure-table-filter {
-    .el-table-filter__list {
-      min-width: 80px;
-      padding: 0;
+.pure-table-filter {
+  .el-table-filter__list {
+    min-width: 80px;
+    padding: 0;
 
-      li {
-        line-height: 28px;
-      }
+    li {
+      line-height: 28px;
     }
   }
+}
 </style>
 
 <style lang="scss" scoped>
-  :deep(.el-table) {
-    --el-table-border: none;
-    --el-table-border-color: transparent;
+:deep(.el-table) {
+  --el-table-border: none;
+  --el-table-border-color: transparent;
 
-    .el-empty__description {
-      margin: 0;
-    }
-
-    .el-scrollbar__bar {
-      display: none;
-    }
+  .el-empty__description {
+    margin: 0;
   }
+
+  .el-scrollbar__bar {
+    display: none;
+  }
+}
 </style>
