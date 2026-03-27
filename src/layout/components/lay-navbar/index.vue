fbf66b91680f4339c7ca1f2aec0417bacbf79feb
tigerk <kimhwawoon@gmail.com>
2026-03-27 08:20:53 +0800

    Disable `LayNotice` component in navbar

--- src/layout/components/lay-navbar/index.vue
+++ src/layout/components/lay-navbar/index.vue
@@ -132,7 +132,7 @@ const {
       <!-- 主题模式 -->
       <LaySidebarThemeMode id="header-theme-mode" />
       <!-- 消息通知 -->
-      <LayNotice id="header-notice" />
+      <LayNotice v-if="false" id="header-notice" />
       <!-- 退出登录 -->
       <el-dropdown trigger="click">
         <span class="el-dropdown-link navbar-bg-hover select-none">
